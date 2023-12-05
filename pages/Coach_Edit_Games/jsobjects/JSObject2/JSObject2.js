4034866072565971f7dac91e11e35651987324ab
Sion Dweck <ccdweck@gmail.com>
2023-12-05 04:24:46 +0000

    added mad screens

---
+++ pages/Coach_Edit_Games/jsobjects/JSObject2/JSObject2.js
@@ -0,0 +1,12 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		{{ Delete_Game.run({ game_id: Simplified_Games.selectedRow.game_id }) }}
+		
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
