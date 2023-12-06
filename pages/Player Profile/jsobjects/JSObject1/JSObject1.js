3c74b68123b78ccb70470ffb59f4badf8da811a8
Sion Dweck <ccdweck@gmail.com>
2023-12-06 19:30:54 +0000

    fixed buttons

---
+++ pages/Player Profile/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,11 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		 window.open('https://www.draftkings.com', '_blank');
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
