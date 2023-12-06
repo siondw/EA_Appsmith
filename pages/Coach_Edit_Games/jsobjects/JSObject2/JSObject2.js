eafb02c74d97009c63a30892f82b4cecf151e5b0
Ccdwe <dweckspam@gmail.com>
2023-12-06 10:40:18 -0500

    reverting

---
+++ pages/Coach_Edit_Games/jsobjects/JSObject2/JSObject2.js
@@ -0,0 +1,16 @@
+export default {
+    myVar1: [],
+    myVar2: {},
+     async myFun1 () {
+        // Assuming Delete_Game.run() returns a promise
+        await Delete_Game.run({ game_id: Simplified_Games.selectedRow.game_id });
+        
+        // After the delete operation is complete, fetch the updated games list
+        await Get_Simplified_Games.run();
+			return "Game Deleted"
+    },
+    async myFun2 () {
+        // use async-await or promises
+        // await storeValue('varName', 'hello world')
+    }
+}
---
+++ pages/Team_Stats_ANd_Games/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,13 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		{{ storeValue('selectedGameId', Table1.selectedRow.game_id); 
+			navigateTo('Game_Dashboard'); }}
+
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
